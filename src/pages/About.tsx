112e2f18d677c12759c58577325bb506822acafe
shadman15 <58660124+shadman15@users.noreply.github.com>
2025-09-17 22:50:45 +0100

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+const About = () => (
+  <div className="container mx-auto py-8">
+    <h1 className="text-3xl font-bold mb-4">About Aurélise Bakery</h1>
+    <p>We are passionate about artisan cakes and pastries!</p>
+  </div>
+);
+
+export default About;
